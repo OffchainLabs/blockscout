6685900c5dbb6e81187698a999deab9f78aac49c
Adam Clark <aclark@aclark-d190n1.tail6f50.ts.net>
2025-11-04 11:26:43 +0000

    Add a catch all clause

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/log.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/log.ex
@@ -183,6 +183,9 @@ defmodule EthereumJSONRPC.Log do
     end
   end
 
+  defp entry_to_elixir(_) do
+      {:ignore, :ignore}
+
   defp put_topics(params, topics) when is_map(params) and is_list(topics) do
     params
     |> Map.put(:first_topic, Enum.at(topics, 0))
